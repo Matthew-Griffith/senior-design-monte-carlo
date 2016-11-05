22c3c4e48f9a3c354f2e94c021840ce86d846fad
Matthew-Griffith <matt.griff10@gmail.com>
2016-11-04 21:00:18 -0400

    created a version of the analysis using normal distrubution for the randomness instead of uniform. and displayed the data using a histogram.

--- curve_fitting.py
+++ curve_fitting.py
@@ -1,20 +1,23 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy import stats
+# from scipy import stats
+import matplotlib.mlab as mlab
 
 results = np.load('testing.npy')
+time1km = results[:, 1]
+cases = np.arange(0, 1000)
+stdDeviation = np.std(time1km)
+mean = np.mean(time1km)
 
-# Linear regression on the data set
-slope, intercept, r_value, p_value, std_err = stats.linregress(results[:, 0], results[:, 1])
-velRange = np.arange(0.8, 1.2, .001)
-linTime = velRange * slope + intercept
-# print(slope, intercept)
+# plt.plot(cases, time1km, label='Raw Data', marker='.', linestyle='none')
+plt.hist(time1km, bins=45)
 
-plt.plot(results[:, 0], results[:, 1], label='Raw Data', marker='.', linestyle='none')
-plt.plot(velRange, linTime, label='Linear regression', color='red')
+# y = mlab.normpdf(60, mean, stdDeviation)
+# plt.plot(y, 'r--', linewidth=1)
+
 plt.title('Monte Carlo Analysis for Velocity Magnitude')
-plt.xlabel('Velocity [m/s]')
-plt.ylabel('Time within 1 km [secs]')
+plt.xlabel('Time within 1 km [secs]')
+plt.ylabel('cases')
 plt.legend(loc='upper right')
 plt.grid()
 # plt.savefig('1000_samples_lin_regression.png')
