ffd10b05bb745424d5140b92676d9bb8901bdc45
Matthew-Griffith <matt.griff10@gmail.com>
2016-11-09 04:55:51 -0500

    added a curve of best fit for a normal distribution.

--- curve_fitting.py
+++ curve_fitting.py
@@ -1,22 +1,21 @@
 import numpy as np
 import matplotlib.pyplot as plt
-# import matplotlib.mlab as mlab
+import matplotlib.mlab as mlab
 
 results = np.load('testing.npy')
 time1km = results[:, 1]
 cases = np.arange(0, 1000)
-# stdDeviation = np.std(time1km)
-# mean = np.mean(time1km)
+stdDeviation = np.std(time1km)
+mean = np.mean(time1km)
 
-# plt.plot(cases, time1km, label='Raw Data', marker='.', linestyle='none')
-plt.hist(time1km, bins=45)
-
-# y = mlab.normpdf(60, mean, stdDeviation)
-# plt.plot(y, 'r--', linewidth=1)
+plt.hist(time1km, bins=45, normed=True, facecolor='blue')
+x = np.sort(time1km)
+y = mlab.normpdf(x, mean, stdDeviation)
+plt.plot(x, y, 'r+', linewidth=1, label='Normal Distribution')
 
 plt.title('Monte Carlo Analysis for Velocity Magnitude')
 plt.xlabel('Time within 1 km [secs]')
-plt.ylabel('cases')
+plt.ylabel('Cases')
 plt.legend(loc='upper right')
 plt.grid()
 plt.savefig('histogram_velocity_magnitude.png')
