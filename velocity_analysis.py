a0733379ed0ccf75a9b40fc9ee06fe893316a667
Matthew-Griffith <matt.griff10@gmail.com>
2017-01-06 05:30:24 -0500

    fixed the solver for the kepler eqn function. I forgot to actually return the value it found

--- velocity_analysis.py
+++ velocity_analysis.py
@@ -1,6 +1,61 @@
 import numpy as np
-from math import sin, cos, pi, sqrt
+from math import sin, cos, cosh, sinh, pi, sqrt
 from scipy.optimize import newton
+
+def stumpC(z):
+    '''
+    :param z: z = alpha * X^2, where alpha is the reciprocal of the semimajor axis and X is the universal anomaly.
+    :return: the value of the stumpff function C(z) at a given Z, note first you need to find the universal anomally.
+    '''
+    if z > 0:
+        return (1- cos(sqrt(z)))/z
+    elif z < 0:
+        return (cosh(sqrt(-z))-1)/(-z)
+    else:
+        return 1/2
+
+def stumpS(z):
+    '''
+    :param z: z = alpha * X^2, where alpha is the reciprocal of the semimajor axis and X is the universal anomaly.
+    :return: the value of the stumpff function S(z) at a given Z, note first you need to find the universal anomally.
+    '''
+    if z > 0:
+        return (sqrt(z) - sin(sqrt(z)))/((sqrt(z))**3)
+    elif z < 0:
+        return (sinh(sqrt(z)) - sqrt(-z))/((sqrt(-z))**3)
+    else:
+        return 1/6
+
+def kepler_eqn(time, postion, velocity, semiMajorAxis, mu):
+    '''
+    :param time: time at which you are try to find X the universal anomaly at.
+    :param postion: this the magnitude of the initial position vector
+    :param velocity: the magnitude of the initial velocity vector
+    :param semiMajorAxis: the semi-major axis of your orbits eclipse
+    :param mu standard gravity parameter
+    :return: X the universal anomaly at the given time.
+
+    this function uses newton's method to solve for universal anomaly from kepler's equation. a iterative method is used
+    here, the other options would be to use a graphical option or an approximation. But given that I want to make this
+    analysis as general as possible this method seems the best and this can be reused to later analysis because of that.
+    '''
+
+    error = 1 * 10**(-8)
+    maxIter = 1000
+    x = sqrt(mu) * abs(semiMajorAxis) * time    # starting point
+    alpha = 1/semiMajorAxis
+    n = 0
+    ratio = 1
+    while abs(ratio) > error and n <= maxIter:
+        n += 1
+        C = stumpC(alpha*x**2)
+        S = stumpS(alpha*x**2)
+        F = postion * velocity / sqrt(mu) * x ** 2 * C + (1 - alpha * postion) * x ** 3 * S + postion * x - sqrt(mu) * time
+        dFdx = postion * velocity / sqrt(mu) * x * (1 - alpha * x ** 2 * S) + (1 - alpha * postion) * x ** 2 * C + postion
+        ratio = F/dFdx
+        x = x - ratio
+
+    return x
 
 # here we create a var for how many times we want to in the model
 samples = 1032
@@ -34,6 +89,9 @@ vz_sep = velMag*np.cos(phi)
 stdGravPar = 398600             # (km^3)/(s^2)
 radiusInitial = 6378 + 500      # km
 angMoCombine = sqrt(radiusInitial*stdGravPar)   # angular momentum for initial orbit km^3/s^2
-velTanInitial = angMoCombine/radiusInitial      # tangential velocity km/s
+velTan = angMoCombine/radiusInitial      # tangential velocity km/s
 posCombine = np.array([[radiusInitial], [0], [0]])  # combine position vector at t = 0
-velCombine = np.array([[0], [velTanInitial], [0]])  # combine velocity vector at t = 0
+velCombine = np.array([[0], [velTan], [0]])  # combine velocity vector at t = 0
+
+testing = kepler_eqn(250, radiusInitial, velTan, radiusInitial, stdGravPar)
+print(testing)
